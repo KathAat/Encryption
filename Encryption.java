1014fe3cb3b12342f383a946f9e1f60ece1e26a9
Kat <katie2563507@gmail.com>
2023-10-13 08:49:00 -0400

    Encryption

---
+++ Encryption.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Encryption {
+ ArrayList<Character> symbols = new ArrayList<Character>();
+ ArrayList<Character> alphabets = new ArrayList<Character>();
+ public Encryption() {
+ symbols.add('!');
+ symbols.add('@');
+ symbols.add('#');
+ symbols.add('$');
+ symbols.add('^');
+ symbols.add('&');
+ symbols.add('*');
+ symbols.add('(');
+ symbols.add(')');
+ symbols.add('_');
+ symbols.add('-');
+ symbols.add('+');
+ symbols.add('=');
+ symbols.add('?');
+ symbols.add(',');
+ symbols.add('{');
+ symbols.add('}');
+ symbols.add('[');
+ symbols.add(']');
+ symbols.add('/');
+ symbols.add('|');
+ symbols.add(';');
+ symbols.add(':');
+ symbols.add('.');
+ symbols.add('<');
+ symbols.add('>');
+ for(char letter = 'a'; letter <= 'z'; letter++) {
+ alphabets.add(letter);
+ }
+ }
+
+
+ public class alphabets {
+ public alphas() {
+ symbols.add('a');
+ symbols.add('b');
+ symbols.add('c');
+ symbols.add('d');
+ symbols.add('e');
+ symbols.add('f');
+ symbols.add('g');
+ symbols.add('h');
+ symbols.add('i');
+ symbols.add('j');
+ symbols.add('k');
+ symbols.add('l');
+ symbols.add('m');
+ symbols.add('n');
+ symbols.add('o');
+ symbols.add('p');
+ symbols.add('q');
+ symbols.add('r');
+ symbols.add('s');
+ symbols.add('t');
+ symbols.add('u');
+ symbols.add('v');
+ symbols.add('w');
+ symbols.add('x');
+ symbols.add('y');
+ symbols.add('z');
+ for(char alpha = '!'; alpha <= '>'; alpha++) {
+ symbols.add(alpha);
+ }
+ }
+ public class returnindex() {
+    
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+    
+    
+
+ 
+
+
+
+
